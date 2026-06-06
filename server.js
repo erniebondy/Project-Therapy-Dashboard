581e14c73c84bf14b1e882a8b1b18f9d0444a4bf
Ernest Bondy <erniebondy@hotmail.com>
2026-06-06 12:54:18 -0700

    env port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const sql = require('sqlite3');
 const crypto = require('node:crypto');
 
 const app = express();
-const port = 3000; //import.meta.env.VITE_PORT;
+const port = process.env.PORT || 3000; //import.meta.env.VITE_PORT;
 const dbPath = './db/dev.sqlite3';
 
 // Middleware
